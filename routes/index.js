62802fe00008b22b7df46e35040914ad098498d5
kilgette <kilgette@gmail.com>
2023-05-27 18:27:39 -0500

    updated routes with instructor express starter kit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+//Gary's starter kit code 
+
+// const router = require("express").Router()
+// const otherRoutes = require("./otherRoutes");
+// const userRoutes = require("./userRoutes");
+
+
+// router.use("/other", otherRoutes);
+// router.use("/user", userRoutes);
+
+
+// module.exports = router;
